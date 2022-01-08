9633c2f503778c876f889352e38efa804fae186c
oabdulla <oabdulla@heslb.go.tz>
2022-01-08 20:32:17 +0300

    district and regions

--- app/Districts.php
+++ app/Districts.php
@@ -7,7 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class districts extends Model
 {
     //
+    public $table='district';
+    protected $primaryKey='dist_id';
+    const UPDATED_AT = 'UPDATED_DATE';
+    const  CREATED_AT = 'CREATED_DATE';
+    public $fillable = [
+        'dist_id',
+        'dist_name',
+        'reg_id'
+    ];
+    protected $casts = [
+        'dist_id'=>'integer',
+        'dist_name'=>'string',
+        'reg_id'=>'integer'
+    ];
+    public static  $rules = [
+        'dist_name'=>'required',
+        'reg_id'=>'required'
+    ];
     public  function village(){
-        return $this->hasMany(villages::class);
+        return $this->hasMany(Villages::class);
     }
 }
--- app/Regions.php
+++ app/Regions.php
@@ -7,7 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class regions extends Model
 {
     //
+    public $table ='regions';
+    protected $primaryKey ='reg_id';
+    const CREATED_AT = 'CREATED_DATE';
+    const UPDATED_AT = 'UPDATED_DATE';
+    public $fillable = [
+        'reg_id',
+        'reg_name'
+        ];
+    protected $casts =[
+        'reg_id'=>'integer',
+        'reg_name'=>'string'
+    ];
+    public static $rules = [
+        'reg_name' =>'required'
+    ];
     public function district(){
-        return $this->hasMany(districts::class);
+        return $this->hasMany(Districts::class);
     }
 }
